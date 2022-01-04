62fbcc1da4a6341d8b370c882795e70bd423d9cf
Rki0 <86224851+Rki0@users.noreply.github.com>
2022-01-04 20:33:40 +0900

    Delete 바닐라js로 크롬 앱 만들기 directory

--- 바닐라js로 크롬 앱 만들기/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.png", "1.jfif", "2.jfif", "3.jfif", "4.png"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `images/${chosenImage}`;
-
-document.body.appendChild(bgImage);
