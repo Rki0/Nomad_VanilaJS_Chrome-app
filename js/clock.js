7d46b823fc980b9627c586b75fbde664cfbd727b
Rki0 <86224851+Rki0@users.noreply.github.com>
2022-01-11 00:15:33 +0900

    add checkbox and  strikethrough
    if you checked the checkbox, todoList will be covered with strikethrough.

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,28 @@
 const clock = document.querySelector("#clock");
 
 function getClock(){
-  const date = new Date();
-  const hour = String(date.getHours()).padStart(2,0);
-  const min = String(date.getMinutes()).padStart(2,0);
-  const sec = String(date.getSeconds()).padStart(2,0);
+  const time = new Date();
+  const hour = String(time.getHours()).padStart(2,0);
+  const min = String(time.getMinutes()).padStart(2,0);
+  const sec = String(time.getSeconds()).padStart(2,0);
   clock.innerText = `${hour}:${min}:${sec}`
 }
 
 getClock();
 setInterval(getClock,1000);
+
+/**Add today's date upward clock */
+const calendar = document.querySelector("#date");
+
+function getDate(){
+  const date = new Date();
+  const year = date.getUTCFullYear();
+  let month = date.getMonth();
+  month = month + 1;
+  const stringMonth = String(month).padStart(2,0);
+  const day = date.getDate();
+  calendar.innerText = `${year}-${stringMonth}-${day}`;
+}
+
+getDate();
+
